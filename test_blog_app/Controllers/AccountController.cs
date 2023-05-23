41c27f8e53a379528f2ce1276af3c9d87b0f131a
endliar <endliar@yandex.ru>
2023-05-23 13:10:43 +0700

    Добавьте файлы проекта.

---
+++ test_blog_app/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test_blog_app.Models;
+
+namespace test_blog_app.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(UserRegistrationModel userModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ test_blog_app/Models/UserRegistrationModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace test_blog_app.Models
+{
+    public class UserRegistrationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress] 
+        public string EmailAddress { get; set;}
+
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Phone is required")]
+        [DataType(DataType.PhoneNumber)] 
+        public string PhoneNumberConfirmed { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation do not match")]
+        public string ConfirmPassword { get; set;}
+    }
+ }
