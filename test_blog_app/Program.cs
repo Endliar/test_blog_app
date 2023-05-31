704f2f33a358751188e676648ec09a01dcdaeac6
endliar <endliar@yandex.ru>
2023-05-31 12:59:56 +0700

    Оно работает, это всё.

--- test_blog_app/Program.cs
+++ test_blog_app/Program.cs
@@ -1,25 +1,25 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using test_blog;
 using test_blog_app.Data;
 using test_blog_app.Models;
+using test_blog_app.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-/*
+
 var connectionString = builder.Configuration.GetConnectionString("SecurityDb") ?? throw new InvalidOperationException("Connection string 'SecurityDb' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("SecurityDb")));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+options.UseNpgsql(builder.Configuration.GetConnectionString("SecurityDb")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-*/
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+.AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<UserDataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("SecurityDb")));
 
 var app = builder.Build();
@@ -27,13 +27,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+app.UseExceptionHandler("/Home/Error");
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -44,8 +44,73 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
+
+
+
+/*
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<UserDataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("SecurityDb")));
+
+
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+
+app.Run();
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.ConfigureCors();
+        services.ConfigureIISIntegration();
+
+        services.AddControllers();
+        services.AddRazorPages();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseCors("CorsPolicy");
+
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.All
+        });
+
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+} 
+*/
